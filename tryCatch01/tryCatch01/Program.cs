511f08017cf78edee8f7436501dd5fd0c281f4c1
Neven <58472164+neven14@users.noreply.github.com>
2020-11-16 08:59:38 +0100

    Add files via upload

---
+++ tryCatch01/tryCatch01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatch01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+            Console.Write("Unesi broj: ");
+
+            try
+            {
+                broj = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(broj);
+            }
+            catch (Exception greska )
+            {
+                Console.WriteLine(greska.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
